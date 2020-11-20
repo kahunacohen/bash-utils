bc1c2853f72c3891683936093601e7a03294655c
Aaron Cohen <aaron.samuel.cohen@gmail.com>
2020-11-20 13:43:37 +0200

    add leading zeros

--- git.sh
+++ git.sh
@@ -1,13 +1,10 @@
-### Increments the part of the string
-## $1: version itself
-## $2: number of part: 0 – major, 1 – minor, 2 – patch
-# Riff on https://stackoverflow.com/questions/8653126/how-to-increment-version-number-in-a-shell-script/17364637#64390598
+#!/usr/bin/env bash
 
-#  declare part=${1:-$(</dev/stdin)};
-#  declare ver=${2:-$(</dev/stdin)};
-bump_version() {
+function bump_version {
   local delimiter=.
   local array=($(echo "$1" | tr $delimiter '\n'))
   array[$2]=$((array[$2]+1))
+  if [ $2 -lt 2 ]; then array[2]=0; fi
+  if [ $2 -lt 1 ]; then array[1]=0; fi
   echo $(local IFS=$delimiter ; echo "${array[*]}")
 }
