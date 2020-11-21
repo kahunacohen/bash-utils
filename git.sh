33e6e1d51db6312943c9b259ecc29be091244236
Aaron Cohen <aaron.samuel.cohen@gmail.com>
2020-11-21 20:41:24 +0200

    bump working

--- git.sh
+++ git.sh
@@ -4,12 +4,17 @@
 ## $1: version itself
 ## $2: number of part: 0 – major, 1 – minor, 2 – patch
 function bump_version {
-  declare version=${1:-$(</dev/stdin)};
-  declare part=${2:-$(</dev/stdin)};
+    declare version=${1:-$(</dev/stdin)};
+      declare part=${2:-$(</dev/stdin)};
+    local i=2 # Default is patch
+  if [ "major" == "$part" ]; then i=0; fi
+  if [ "minor" == "$part" ]; then i=1; fi
+
+
   local delimiter=.
   local array=($(echo "$version" | tr $delimiter '\n'))
-  array[$2]=$((array[$part]+1))
-  if [ $2 -lt 2 ]; then array[2]=0; fi
-  if [ $2 -lt 1 ]; then array[1]=0; fi
+  array[$i]=$((array[$i]+1))
+  if [ $i -lt 2 ]; then array[2]=0; fi
+  if [ $i -lt 1 ]; then array[1]=0; fi
   echo $(local IFS=$delimiter ; echo "${array[*]}")
 }
