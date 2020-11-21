73fd043f283e19561af789e48725e7b8652be19d
Aaron Cohen <aaron.samuel.cohen@gmail.com>
2020-11-21 23:11:25 +0200

    Add gca

--- git.sh
+++ git.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
+function gca {
+  git commit -am "$1"
+}
 ### Increments the part of the string
 ## $1: version itself
 ## $2: part: major, minor, patch
 function bump_version {
-    declare version=${2:-$(</dev/stdin)};
-      declare part=${1:-$(</dev/stdin)};
-    local i=2 # Default is patch
+  declare part=${1:-$(</dev/stdin)};
+  declare version=${2:-$(</dev/stdin)};
+  local i=2 # Default is patch
   if [ "major" == "$part" ]; then i=0; fi
   if [ "minor" == "$part" ]; then i=1; fi
-
-
   local delimiter=.
   local array=($(echo "$version" | tr $delimiter '\n'))
   array[$i]=$((array[$i]+1))
