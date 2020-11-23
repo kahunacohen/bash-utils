2396a3de377030a34bb85c44ddeed6cbe63ff4a6
Aaron Cohen <aaron.samuel.cohen@gmail.com>
2020-11-23 19:10:57 +0200

    Add fns.sh

--- node.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-# Outputs package.json version with new version
-# $1 - The path to package.json. E.g. ./
-# $2 - The version
-function mod_package_json {
-    jq ".version=\"$2\"" $1/package.json
-}
